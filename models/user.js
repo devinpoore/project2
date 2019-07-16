e3edc89be917d0ade030d33711808968db8bf130
Monica Parente <mon8806@gmail.com>
2019-07-16 10:42:12 -0700

    added database

---
+++ models/user.js
@@ -0,0 +1,52 @@
+var bcrypt = require('bcrypt');
+
+var Listing = require('./listings')
+
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define("user", {
+    id:{
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+      isUnique: true
+    },  
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },  
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isEmail: true,
+      isUnique: true,
+    },
+    phoneNumber: {
+      type: DataTypes.INTEGER,
+      required: true,
+      len: [9,9]
+    },
+    streetAddress:{
+      type: DataTypes.INTEGER,
+      required: true
+    },
+    password:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+      len: [5,12]
+    },
+  });
+
+  user.hasMany(Listing)
+  return user;
+};
