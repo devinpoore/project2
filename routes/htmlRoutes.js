0922c4f80c6e7bf994e919d4866fe284f60e4b36
Everett Smith <evepsmith15@gmail.com>
2019-07-17 11:24:59 -0700

    fixing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,18 +3,8 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-<<<<<<< HEAD
     res.render("index");
 
-=======
-  /*  db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });*/
-    res.render("index")
->>>>>>> es
   });
 
   // Load example page and pass in an example by id
